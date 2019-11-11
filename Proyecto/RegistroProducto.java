418e4208f985c4d69d3aa5fc1d455ef1ce4b0568
Anita <is4iaschav3z@gmail.com>
2019-11-11 13:46:32 -0600

    Se implementaron los métodos de escritura y lectura de Productos

--- Proyecto/RegistroProducto.java
+++ Proyecto/RegistroProducto.java
@@ -1,8 +1,12 @@
 package Proyecto;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class RegistroProducto {
 
@@ -38,9 +42,58 @@ public class RegistroProducto {
 		
 	}
 	public void escribirArchivo(ArrayList<Producto> listaProductos) {
+		FileWriter fichero = null;
+		PrintWriter impresor = null;
+		
+		try {
+			fichero = new FileWriter("C:\\Users\\Isaías Chávez\\Documents\\productos.txt");
+			impresor = new PrintWriter(fichero);
+			
+			Iterator<Producto> i = listaProductos.iterator();
+			Producto producto;
+			while(i.hasNext()) {
+				producto = i.next();
+				impresor.println(producto.getIdProducto()+"::"+producto.getNombre()+"::"+producto.getCantidad()+"::"+producto.getPrecioDeCompra()+"::" +producto.getPrecioDeVenta());
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(fichero !=  null) {
+					fichero.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 		
 	}
 	public void leerArchivo(ArrayList<Producto> listaProductos) {
+		File archivo = null;
+		Scanner lector = null;
+		listaProductos.clear();
+		try {
+			archivo = new File("C:\\Users\\Isaías Chávez\\Documents\\productos.txt");	
+			lector = new Scanner(archivo);
+			String linea;
+			do {
+				linea = lector.nextLine();
+				if(linea!= null) {
+				System.out.println(linea);
+				String[] cortarString = linea.split("::");
+				Producto productoArchivo = new Producto(Integer.parseInt(cortarString[0]),cortarString[1],Integer.parseInt(cortarString[2]),Float.parseFloat(cortarString[3]),Float.parseFloat(cortarString[4]));
+				listaProductos.add(productoArchivo);
+				}
+				
+			} while (linea != null);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+		}
 		
 	}
 	
